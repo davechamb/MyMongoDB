5783cc7bd377eac1712044ad0267c241c7b566e7
davechamb <david.chamberlain@lumimobile.com>
2014-05-22 16:54:38 +0100

    Update default.rb

--- mongodb-mysite/default.rb
+++ mongodb-mysite/default.rb
@@ -1,11 +1,2 @@
-#log "mongodb default.rb"
-
 include_recipe "mongodb::10gen_repo"
-#include_recipe "mongodb::replicaset"
 include_recipe "mongodb::default"
-
-#log "mongodb replicaset"
-
-#mongodb_instance "mongodb" do
-#  replSet rs0
-#end
