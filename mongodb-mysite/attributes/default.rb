55593f2960ca6c78b81518d46d8a45fe6d29df65
davechamb <david.chamberlain@lumimobile.com>
2014-05-23 16:22:49 +0100

    Update default.rb

--- mongodb-mysite/attributes/default.rb
+++ mongodb-mysite/attributes/default.rb
@@ -1,3 +1,4 @@
-node.default[:mongodb][:cluster_name] = 'rs_my_cluster'
-node.default[:mongodb][:config][:replSet] = 'rs_my_cluster'
-node.default['mongodb']['replicaset_name'] ='rs_my_cluster'
+node.default[:mongodb][:cluster_name] = 'rs0'
+node.default[:mongodb][:config][:replSet] = 'rs0'
+node.default[:mongodb][:replicaset_name] ='rs0'
+node.default[:mongodb][:auto_configure][:replicaset] = true
